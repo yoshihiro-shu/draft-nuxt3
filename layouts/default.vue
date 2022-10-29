a8ab1ce500329127550ecd5890f9afc651fe8a98
yoshihiroshu <yoshihiro.shu@zuuonline.com>
2022-10-29 21:24:20 +0900

    add default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NavBar />
+    <Header />
+    <TopicNav />
+    <NuxtPage />
+    <div class="container mx-auto flex flex-wrap py-6">
+      <!-- <Article /> -->
+      <SideBar />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+</script>
